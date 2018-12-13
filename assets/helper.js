d8334a0ac9441423a05f59b9d28d97c502706465
Jean Pierre Sosa <jpsosa@virtualmind.net>
2018-12-13 16:40:15 -0300

    Fdfgggb

--- assets/helper.js
+++ assets/helper.js
@@ -1,10 +1,10 @@
 const response =  (replace, params, paramService, callback) => {
   let response = {
     'fulfillmentText': '',
-    'outputContexts': ''
+    'outputContexts': [paramService.outputContexts[ paramService.outputContexts.length-1]]
 
   };
-//  console.log('xxxxxxxxxxxxxxx',  paramService);
+  console.log('xxxxxxxxxxxxxxx', response);
   for (let x = 0, n = replace.length; x < n; x++ ) {
     response['fulfillmentText'] =  paramService.inputText.replace((new RegExp(replace[x] || '', 'g') ), params[x]);
   }
