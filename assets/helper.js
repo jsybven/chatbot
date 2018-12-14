f136a6bb54a32cfe6684b9d610f09acf3f881c0e
Jean Pierre Sosa <jpsosa@virtualmind.net>
2018-12-14 15:53:21 -0300

    dfggg55555

--- assets/helper.js
+++ assets/helper.js
@@ -1,13 +1,13 @@
 const response =  (replace, params, paramService, callback) => {
   let response = {
     'fulfillmentText': 'tataatt',
-    'outputContexts': [/*paramService.outputContexts[ paramService.outputContexts.length-1]*/]
+    'outputContexts': [paramService.outputContexts[ paramService.outputContexts.length-1]]
 
   };
   delete paramService.todo.queryResult.outputContexts;
   console.log('xxxxxxxxxxxxxxx', response);
   for (let x = 0, n = replace.length; x < n; x++ ) {
-     response['fulfillmentText'] =  paramService.inputText.replace((new RegExp(replace[x] || '', 'g') ), params[x]);
+  //   response['fulfillmentText'] =  paramService.inputText.replace((new RegExp(replace[x] || '', 'g') ), params[x]);
   // paramService.todo.queryResult.parameters =  paramService.inputText.replace((new RegExp(replace[x] || '', 'g') ), params[x]);
   }
   callback.send(response);
