7a9082014999b348722ed38f5d6c6b91b33dad30
Jean Pierre Sosa <jpsosa@virtualmind.net>
2018-12-17 14:41:50 -0300

    SDfdf

--- assets/helper.js
+++ assets/helper.js
@@ -5,7 +5,7 @@ const response =  (replace, params, paramService, callback) => {
   "languageCode": "en"
 
   };
-  response.outputContexts[0].lifespanCount = 0;
+  //response.outputContexts[0].lifespanCount = 0;
   //delete paramService.todo.queryResult.outputContexts;
   console.log('xxxxxxxxxxxxxxx', response);
   for (let x = 0, n = replace.length; x < n; x++ ) {
