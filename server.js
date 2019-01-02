74d22f03a39c13124a578a7bcbff83f15f37cd66
Jean Pierre Sosa <jpsosa@virtualmind.com>
2019-01-02 17:47:58 -0300

    redis

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const express = require("express"),
       request = require('request'),
       fs = require('fs'),
       dialogflow = require('dialogflow'),
-      { WebClient } = require('@slack/client');
-      const structjson = require('./structjson.js');
+      redis = require('redis'),
+      JSONCache = require('redis-json'),
+      { WebClient } = require('@slack/client'),
+      structjson = require('./structjson.js');
 
       const tokenPruebas = '';
 
@@ -65,7 +67,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-var router = express.Router();
+let router = express.Router();
+
+let clientRedis = redis.createClient();
+
+clientRedis.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+//const jsonRedis = new JSONCache(redis, {prefix: 'cache:'});
 
 router.get('/', function(req, res) {
    res.send("hablame el mio!");
@@ -76,10 +86,10 @@ router.post('/bot', function(req, res) {
   // console.log(req.body.originalDetectIntentRequest.payload.data);
   const action = req.body.queryResult.action;
 
-  console.log(req.body);
+/*  console.log(req.body);
   console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
   console.log(req.body.originalDetectIntentRequest);
-  console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+  console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");*/
 /*  console.log(req.body.queryResult.fulfillmentMessages);
   console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");*/
   let param = {
@@ -100,37 +110,53 @@ router.post('/bot', function(req, res) {
 
 
 
-router.post('/slacky', function(req, res) {
+router.post('/middleware', function(req, res) {
   const en = "https://bots.dialogflow.com/slack/2a02c9ec-de56-4e7e-b103-bb7f6f6adcba/webhook";
   const es = 'https://bots.dialogflow.com/slack/7f86df03-1d7c-4238-ba5f-adfb9247116b/webhook';
   let body = {
       url: es,
       form: req.body
   }
- //console.log( req.body);
-//console.log( "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-
-//console.log("qqqqq", req.body.event.upload , req.body.event.client_msg_id);
-if (req.body.event.client_msg_id || req.body.event.upload) {
-  //console.log("qqqqq", req.body.event.upload);
-  if (req.body.event.upload) {
-
-      body.form.event.text = req.body.event.files[0].url_private;
-    //  console.log(body.form.event.text);
-      const fileName = body.form.event.text.split("/");
-      //  este codigo es para descargar el archivo
-        request.get({
-                    url: body.form.event.text,
-                    headers: { 'Content-Type': 'application/json',
-                               'Authorization': 'Bearer'
-                             }
-                    }).on( 'response', function( res ){
-            res.pipe(fs.createWriteStream( './temp/' + fileName[fileName.length-1] ));
-         });
-  }
+  if (req.body.event.client_msg_id || req.body.event.upload) {
+    if (req.body.event.upload) {
+        body.form.event.text = req.body.event.files[0].url_private;
+        const fileName = body.form.event.text.split("/");
+          request.get({
+                      url: body.form.event.text,
+                      headers: { 'Content-Type': 'application/json',
+                                 'Authorization': 'Bearer'
+                               }
+                      }).on( 'response', function( res ){
+              res.pipe(fs.createWriteStream( './temp/' + fileName[fileName.length-1] ));
+           });
+    }
 
     inputRequest.session = sessionClient.sessionPath(agenteID, req.body.event.channel);
     inputRequest.queryInput.text.text = req.body.event.text;
+
+    clientRedis.hgetall(req.body.event.user, function(err, result) {
+      console.log(JSON.stringify(result)); // {"key":"value","second key":"second value"}
+      if(!result) {
+        slackUserInfo(req.body.event.user);
+      }
+    });
+  //  console.log(jsonCache.get(req.body.event.user));
+/*  clientRedis.hget(req.body.event.user, (err, res) => {
+    clientRedis.watch(req.body.event.user, function( err ){
+        if(err) throw err;
+
+         /**
+         * WRONG: This is now watching the keys 'foo' and 'hello'. It is not
+         * watching the field 'hello' of hash 'foo'. Because the key 'foo'
+         * refers to a hash, this command is now watching the entire hash
+         * for modifications.
+
+    });
+    console.log('aaaaaaaaawww',res);
+    if(!res) {
+      slackUserInfo(req.body.event.user);
+    }
+  });*/
     inputRequest.queryParams.payload = structjson.jsonToStructProto(
         req.body
       )
@@ -142,6 +168,18 @@ if (req.body.event.client_msg_id || req.body.event.upload) {
     "challenge": req.body.challenge
   });*/
 });
+
+
+function slackUserInfo (user){
+  request.get(`https://slack.com/api/users.info?token=${token}&user=${user}&pretty=1`, function( error, response, body ){
+    //  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", body);
+    body = JSON.parse(body);
+//console.log( body.user.real_name );
+      clientRedis.hmset(user, ['name', body.user.real_name,
+      'email', body.user.profile.email]);
+    //  clientRedis.set(user,'email', body.user.profile.email,redis.print);
+   });
+}
 
 router.get('/slacky', function(req, res) {
   console.log(req.body);
